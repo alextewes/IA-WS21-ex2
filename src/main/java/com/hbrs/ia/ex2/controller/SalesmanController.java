55b016342c2d780d7815ae802d32e36d3569a3de
mariohoenighausen <mario.hoenighausen@smail.inf.h-brs.de>
2021-10-31 10:16:49 +0100

    Added Salesman (entity), SalesmanController and SalesmanRepository

---
+++ src/main/java/com/hbrs/ia/ex2/controller/SalesmanController.java
@@ -0,0 +1,54 @@
+package com.hbrs.ia.ex2.controller;
+
+import com.hbrs.ia.ex2.model.Salesman;
+import com.hbrs.ia.ex2.repositories.SalesmanRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
+
+//enabling Cors https://www.baeldung.com/spring-cors
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
+@RestController
+public class SalesmanController {
+
+    List<Salesman> sm = new ArrayList<>();
+
+    @Autowired
+    SalesmanRepository salesmanRepository;
+
+    @GetMapping("/")
+    public String index() {
+        return "Salesman API Works";
+    }
+
+    @GetMapping("/api/salesman/{id}")
+    public Salesman getSalesman(@PathVariable int id) {
+        return salesmanRepository.findSalesmanBySid( id);
+    }
+    @GetMapping("/api/salesmen")
+    public List<Salesman> getSalesmen() {
+        return salesmanRepository.findAll();
+    }
+    @PostMapping("/api/salesman")
+    public Salesman createSalesman(@Valid @RequestBody Salesman salesman){
+        salesmanRepository.insert(salesman);
+        return salesman;
+    }
+    @PatchMapping("/api/salesman")
+    public Salesman updateSalesman(@Valid @RequestBody Salesman salesman){
+        Salesman s = salesmanRepository.findSalesmanBySid( salesman.getSid());
+        s.setFirstName(salesman.getFirstName());
+        s.setLastName(salesman.getLastName());
+        salesmanRepository.save(s);
+        return s;
+    }
+    @DeleteMapping("/api/salesman/{sid}")
+    public Salesman deleteSalesman(@PathVariable int sid){
+        Salesman s = salesmanRepository.findSalesmanBySid( sid);
+        salesmanRepository.deleteById("" + sid);
+        return s;
+    }
+}
---
+++ src/main/java/com/hbrs/ia/ex2/model/Salesman.java
@@ -0,0 +1,68 @@
+package com.hbrs.ia.ex2.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "salesman")
+public class Salesman{
+    private int sid;
+    private String firstName;
+    private String lastName;
+    private String dob;
+    private String experience;
+
+    public Salesman(int sid, String firstName, String lastName) {
+        this.sid = sid;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public int getSid() {
+        return sid;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setSid(int sid) {
+        this.sid = sid;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getExperience() {
+        return experience;
+    }
+
+    public void setExperience(String experience) {
+        this.experience = experience;
+    }
+
+    @Override
+    public String toString() {
+        return "Salesman{" +
+                "id=" + sid +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}
