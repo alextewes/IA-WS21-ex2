228a97ae42a1b6485509ed6d5fc178532630c0cd
alexander tewes <alexander.tewe@smail.inf.h-brs.de>
2021-11-02 18:03:43 +0100

    test commit

--- src/main/java/com/hbrs/ia/ex2/repositories/SalesmanRepository.java
+++ src/main/java/com/hbrs/ia/ex2/repositories/SalesmanRepository.java
@@ -3,6 +3,7 @@ package com.hbrs.ia.ex2.repositories;
 import com.hbrs.ia.ex2.model.Salesman;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+
 public interface SalesmanRepository extends MongoRepository<Salesman, String> {
     Salesman findSalesmanBySid(int sid);
 
